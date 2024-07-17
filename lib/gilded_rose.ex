a0b96de68355366f47708dba503947f5611a009d
Tim Matti <tmatti56@gmail.com>
2024-07-17 16:22:36 -0400

    break out sulfuras

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -49,39 +49,26 @@ defmodule GildedRose do
     %{item | quality: item.quality + quality_bump, sell_in: item.sell_in - 1}
   end
 
+  def update_quality(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
+    %{item | quality: 80}
+  end
+
   def update_quality(item) do
     item =
       if item.quality > 0 do
-        if item.name != "Sulfuras, Hand of Ragnaros" do
-          %{item | quality: item.quality - 1}
-        else
-          item
-        end
+        %{item | quality: item.quality - 1}
       else
         item
       end
 
-    item =
-      cond do
-        item.name != "Sulfuras, Hand of Ragnaros" ->
-          %{item | sell_in: item.sell_in - 1}
-
-        true ->
-          item
-      end
+    item = %{item | sell_in: item.sell_in - 1}
 
     item =
       cond do
         item.sell_in < 0 ->
           cond do
             item.quality > 0 ->
-              cond do
-                item.name != "Sulfuras, Hand of Ragnaros" ->
-                  %{item | quality: item.quality - 1}
-
-                true ->
-                  item
-              end
+              %{item | quality: item.quality - 1}
 
             true ->
               item
